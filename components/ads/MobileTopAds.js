2e62344fada0d0cbb01129f72d95d2c1d2536ac5
noonchaa <enouchiha11@gmail.com>
2021-06-24 01:17:36 +0700

    remove sticky

--- components/ads/MobileTopAds.js
+++ components/ads/MobileTopAds.js
@@ -2,7 +2,7 @@ import Image from 'next/image'
 
 const MobileTopAds = ({img,link}) => {
     return(
-        <section className='bg-gray-200 w-full md:hidden sticky top-0 z-0'>
+        <section className='bg-gray-200 w-full md:hidden'>
             <a href={link} target='blank'>
             <Image src={img} layout='responsive' priority={true} width={300} height={100} alt='advertisment'/>
             </a>
--- components/ads/TabletTopAds.js
+++ components/ads/TabletTopAds.js
@@ -2,7 +2,7 @@ import Image from 'next/image'
 
 const TabletTopAds = ({img,link}) => {
     return(
-        <section className='bg-gray-200 w-full hidden md:block sticky top-14 z-0'>
+        <section className='bg-gray-200 w-full hidden md:block'>
             <a href={link} target='blank'>
             <Image src={img} layout='responsive' priority={true} width={728} height={90} alt='advertisment'/>
             </a>
